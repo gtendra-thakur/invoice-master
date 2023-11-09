c27df5dbc0b7f3d67a7692c6a860076ffa5e8a2e
JitendraT <JitendraT@10.130.124.148>
2023-11-09 17:39:33 +0530

    Initial commit

---
+++ src/main/java/com/digitallipi/invoicemaster/InvoicemasterApplication.java
@@ -0,0 +1,13 @@
+package com.digitallipi.invoicemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicemasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicemasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitallipi/invoicemaster/InvoicemasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitallipi.invoicemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicemasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
